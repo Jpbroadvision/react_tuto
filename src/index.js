d6cf7cb5fa48d486e656a5f73765c213aa73cd2a
Jpbroadvision <jpbroadvision2@gmail.com>
2021-12-13 21:47:34 +0000

    Import Componets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//COMPONENTS
+import Home from '../src/components/home';
+import POST from '../src/components/home';
+import Profile from '../src/components/home';
 
 const App = () =>{
 
