97787fcc4b2de54f85669607df33cb4852872730
Jpbroadvision <jpbroadvision2@gmail.com>
2021-12-13 21:31:53 +0000

    Reformated file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () =>{
+
+  return(
+    <div>
+      <h3>Hello there</h3>
+    </div>
+  )
+}
+ReactDOM.render(<App/>, document.querySelector('#root'))
