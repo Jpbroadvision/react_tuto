480c4c49930dc762ae85ac78120dec0e65a421ea
Jpbroadvision <jpbroadvision2@gmail.com>
2021-12-13 13:32:40 +0000

    Add bg coclor

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -1,9 +1,11 @@
 import React from "react"
+import classes from '../css/styles.css'
+
 const NewsListItem = ({ finalNews }) => {
     // make css rules
   
     return (
-        <div>
+        <div className={classes.item_bg}>
             {finalNews.children}
             <h3>
                 {finalNews.title}
