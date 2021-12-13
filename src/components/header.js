b9aec5d1fd95546fc596a40a24d48a884ea63af7
Jpbroadvision <jpbroadvision2@gmail.com>
2021-12-13 10:48:32 +0000

    Refactored with ES6

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,47 @@
-import React from "react";
+//ES5
+// import React from "react"; 
+import React, {Component} from "react"; 
 
-const getYear = () =>{
-    const newDate = new Date();
-    return newDate.getFullYear();
+// const getYear = () => {
+//     const newDate = new Date();
+//     return newDate.getFullYear();
 
+// }
+// from ES5
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 The date year is now {getYear()}
+//             </div>
+//         )
+
+//     }
+// }
+//from ES6
+class Header extends Component {
+    state = {
+        active: false,
+        keywords:"hello"}
+    inputOnchangeHandler = (event) =>{
+        const value = event.target.value ===''? false:true;
+
+        // document.querySelector(".anser").innerHTML =`Hello ${event.target.value}`;
+    this.setState({
+        active: value,
+        keywords: event.target.value
+    })
+    }
+    render() {
+        console.log(this.state.keywords);
+        return (
+            <header style={{background:`${this.state.active ?'red':'blue'}`}}>
+                <div className="logo" >Logo</div>
+                <input type="text" onChange={this.inputOnchangeHandler}/>
+                <p className="anser"></p>
+            </header>
+        )
+
+    }
 }
-const Header = () =>{
-    return <div>The date year is now {getYear()}</div>
-}
-
 export default Header;
