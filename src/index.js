a40206b4534129dc3960cdda9a43a3be66716058
Jpbroadvision <jpbroadvision2@gmail.com>
2021-12-13 10:48:01 +0000

    Rename method to class

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from "react"
+import React, {Component} from "react"
 import reactDom from "react-dom"
+import JSON from './db.json'
 //COMPNENTS
 import Header from "./components/header"
-const App = () =>{
+import NewsList from "./components/news_list"
+class App extends Component {
+  state = {
+    news : JSON
+  }
+  render(){
   return (
     <div>
       <Header/>
+      <NewsList news={this.state.news}/>
     </div>
   )
+  }
 }
 
 reactDom.render(<App/>, document.querySelector('#root'))
