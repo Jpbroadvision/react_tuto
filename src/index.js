faf052d91b3b84f848578f49c52d6cc724c4aaba
Jpbroadvision <jpbroadvision2@gmail.com>
2021-12-25 15:20:24 +0000

    updated with links

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter as Router,Link, Route, Routes} from 'react-router-dom'
 //COMPONENTS
 import Home from './components/home';
 import Post from './components/post';
@@ -8,14 +8,22 @@ import Profile from './components/profile';
 
 const App = () =>{
 
+
   return(
-    <div>
-      <BrowserRouter>
-      <Route path='/home' component = {Home}/>
-      <Route path='/posts' component = {Post}/>
-      <Route path='/profile' component = {Profile}/>
-      </BrowserRouter>
-    </div>
+    <Router>
+        <div>
+          <header>
+             
+            <Link to='/post'>Post</Link><br></br>
+            <Link to='/profile'>Profile</Link>
+          </header>
+        </div>
+      <Routes>
+      <Route path="/" exact component ={<Home />}></Route>
+      <Route path="/post" exact component ={<Post />}></Route>
+      <Route path="/profile" exact component ={<Profile />}></Route>
+      </Routes>
+      </Router>
   )
 }
 ReactDOM.render(<App/>, document.querySelector('#root'))
