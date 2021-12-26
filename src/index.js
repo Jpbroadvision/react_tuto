1708c24e7baa0c441b4aca8fc0d867260f5e4829
Jpbroadvision <jpbroadvision2@gmail.com>
2021-12-26 16:09:42 +0000

    Added useState

--- src/index.js
+++ src/index.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router,Link, Route, Routes} from 'react-router-dom'
-//COMPONENTS
-import Home from './components/home';
-import Post from './components/post';
-import PostItem from './components/post_items';
-import Profile from './components/profile';
+// import {BrowserRouter as Router,Link, Route, Routes} from 'react-router-dom'
+// //COMPONENTS
+// import Home from './components/home';
+// import Post from './components/post';
+// import PostItem from './components/post_items';
+// import Profile from './components/profile';
 
-const App = () =>{
+// const lakeList = [
+//   { id: "1", name: "Maud Lake", trailhead: "Maud" },
+//   { id: "2", name: "Echo Lake", trailhead: "Echo" },
+//   { id: "3", name: "Cascade Lake", trailhead: "Cascade" }
+// ];
 
-
-  return(
-    <Router>
-        <div>
-          <header>
-             
-            <Link to='/post'>Post</Link><br></br>
-            <Link to='/profile'>Profile</Link>
-          </header>
-        </div>
-      <Routes>
-      <Route path="/" exact component ={<Home />}></Route>
-      <Route path="/post" exact component ={<Post />}></Route>
-      <Route path="/post/:id" exact component ={<PostItem />}></Route>
-      <Route path="/profile" exact component ={<Profile />}></Route>
-      </Routes>
-      </Router>
+const Weather = (props) => {
+  return (
+    <div>The Weather is { props.season }</div>
   )
 }
-ReactDOM.render(<App/>, document.querySelector('#root'))
+const Lakers = (props) => {
+  return (
+    <div>
+      The Weather is {props.season}
+    </div>
+  )
+}
+const App = (props) => {
+  const [status, setStatus] = useState("Closed");
+  return (
+    <div>
+      The App is {status}
+      {props.reason ==="good" ? <Weather season ="cold"/>: <Lakers  season="hot"/>}
+      <button onClick={() => setStatus("Open")}>Open</button>
+      <button onClick={() => setStatus("Closed")}>Close</button>
+    </div>
+  )
+}
+ReactDOM.render(<App reason="bad"/>, document.querySelector('#root'))
